da2af97f5208b8b7173366917ce7a542d819f277
4pipo <dogukan28akmaz@gmail.com>
2025-11-14 21:58:12 +0300

    added ui and styling

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
@@ -25,9 +25,22 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.appContainer}>
-      <View style={styles.contentContainer}>
-        <Text style={styles.title}>My Todo List</Text>
+
+    <View style={styles.contentContainer}>
+      <Text style={styles.title}>My Todo List</Text>
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Add a new task..."
+          onChangeText={taskInputHandler}
+          value={enteredTaskText}
+        />
+        <Button title="Add" onPress={addTaskHandler} />
       </View>
+    </View>
+
+
     </SafeAreaView>
   );
 }
@@ -49,6 +62,24 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     textAlign: 'center',
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+    paddingBottom: 10,
+  },
+  textInput: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    borderRadius: 6,
+    marginRight: 10,
+    fontSize: 16,
+  }
 });
 
 
